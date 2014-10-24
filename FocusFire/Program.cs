387feac372975181ccd860ee570489a52fb6be68
shalzuth <andrew.harazin@gmail.com>
2014-10-23 21:58:10 -0700

    added focus fire

---
+++ FocusFire/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace FocusFire
+{
+    internal class Program
+    {
+        public static Menu Config = new Menu("FocusFire", "FocusFire", true);
+        public static Int32 SequenceId = 1;
+        public static Int32 previousNetId = 0x40000019;
+        public static Int32 newNetId = 0x40000019;
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            Game.OnGameSendPacket += Game_OnGameSendPacket;
+            Config.AddItem(new MenuItem("Size", "Size Percentage").SetValue(new Slider(150, 50, 200)));
+            Config.AddToMainMenu();
+        }
+        static void Game_OnGameSendPacket(GamePacketEventArgs args)
+        {
+            if (args.PacketData[0] == Packet.C2S.SetTarget.Header)
+            {
+                ChangeSize(previousNetId, 1.0f);
+                newNetId = BitConverter.ToInt32(args.PacketData, 9);
+                ChangeSize(newNetId, (Single)(Config.Item("Size").GetValue<Slider>().Value) / 100.0f);
+                previousNetId = newNetId;
+            }
+            else if (args.PacketData[0] == Packet.C2S.UpdateConfirm.Header)
+                SequenceId = BitConverter.ToInt32(args.PacketData, 5) + 1;
+        }
+        private static void ChangeSize(Int32 netId, Single size)
+        {
+            Obj_AI_Hero hero = ObjectManager.GetUnitByNetworkId<Obj_AI_Hero>(netId);
+            if (hero != null)
+            {
+                GamePacket p = new GamePacket(0xC4);
+                p.WriteInteger(0);
+                p.WriteInteger(SequenceId++);
+                p.WriteByte(0x1);
+                p.WriteByte(0x8);
+                p.WriteInteger(netId);
+                p.WriteInteger(0x800);
+                p.WriteByte(0x8);
+                p.WriteFloat(size);
+                p.Process();
+                SequenceId++;
+            }
+        }
+    }
+}
---
+++ FocusFire/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[ assembly: AssemblyTitle("JungleTimerHax") ]
+[ assembly: AssemblyDescription("") ]
+[ assembly: AssemblyConfiguration("") ]
+[ assembly: AssemblyCompany("") ]
+[ assembly: AssemblyProduct("JungleTimerHax")]
+[ assembly: AssemblyCopyright("Copyright ©  2014") ]
+[ assembly: AssemblyTrademark("") ]
+[ assembly: AssemblyCulture("") ]
+
+[ assembly: ComVisible(false) ]
+
+[ assembly: Guid("e6cab8b6-530c-49f3-88e6-f0f6339678d7") ]
+
+[ assembly: AssemblyVersion("1.0.0.0") ]
+[ assembly: AssemblyFileVersion("1.0.0.0") ]
